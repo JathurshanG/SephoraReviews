28f5b23a1a0b9a1880e4ed7528a8685fb082668e
JathurshanG <jathur93@gmail.com>
2024-01-30 11:47:53 +0100

    [Update Spark]

--- script/Process/process.py
+++ script/Process/process.py
@@ -1,13 +1,17 @@
+
 import json
 import pandas as pd 
 from pymongo import MongoClient
 import yaml
 from yaml import SafeLoader
-import plotly.express as px
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SparkSession, SQLContext
-
+from pyspark.sql.functions import when
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 def getconfig() :
     inputFile = "../../inputFiles/config.yaml"
@@ -24,23 +28,49 @@ def processAggregateDate() :
         db = client[db][info]
         dt = pd.DataFrame(list(db.find()))
         dt.drop(columns=['_id'],inplace=True)
-
     dt['prodID'] = dt['targetUrl'].apply(lambda x : x.split("-")[-1].split('?')[0])
     dt["price"] = dt['listPrice'].apply(lambda x : float(x.replace('$',"")))
     dt["skuImages"] =  dt["skuImages"].apply(lambda x : x['imageUrl'])
     dt['variant'] = 1
     groupByDt = dt.groupby(by=['prodID','name',"brand",'Category1', 'Category2','Category3',"size"],as_index=False).agg({"reviews": "max",
-                                                                          "variant" :"count",
-                                                                          "rating" : "max",
-                                                                          "price"  : "max",
-                                                                          "lovesCount" : "max"})
+                                                                        "variant" :"count",
+                                                                        "rating" : "max",
+                                                                        "price"  : "max",
+                                                                        "lovesCount" : "max"})
     groupByDt.sort_values(['prodID'],ascending=[False]).reset_index(drop=True,inplace=True)
-    groupByDt.to_json('../../outputFiles/GroupedData.json',orient='values')
+    groupByDt.to_json('../../outputFiles/dataset/GroupedData.json')
     return groupByDt
 
 def groupedCat(name,rev):
     dt = rev.groupBy(['ProdID']).pivot(name).count().drop('null')
     return dt
+
+
+def LogisticRegressions(rev) :
+    rev = rev.drop('CID').drop('_id').sort('ProdID').dropna(subset='ReviewText')
+    Analysis = rev.filter((rev['Rating']==1) | (rev['Rating']==5))
+    Analysis = Analysis.withColumn('label',when(Analysis['Rating']==5,1).when(Analysis['Rating']==1,0)).limit(100000).select('ProdID','ReviewText','label')
+    # Création d'un pipeline de traitement de texte
+    tokenizer = Tokenizer(inputCol="ReviewText", outputCol="mots")
+    hashingTF = HashingTF(inputCol="mots", outputCol="rawFeatures", numFeatures=10000)
+    idf = IDF(inputCol="rawFeatures", outputCol="features")
+    pipeline = Pipeline(stages=[tokenizer, hashingTF, idf])
+    # Division des données en ensembles d'entraînement et de test
+    train_data, test_data = Analysis.randomSplit([0.8, 0.2], seed=42)
+    # Entraînement du modèle de régression logistique
+    lr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10)
+    pipeline_lr = Pipeline(stages=[pipeline, lr])
+    model = pipeline_lr.fit(train_data)
+    predictions = model.transform(test_data)
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction", labelCol="label")
+    accuracy = evaluator.evaluate(predictions)
+    print(f"Précision du modèle : {accuracy}")
+    AllRev = rev.select('ProdID','ReviewText')
+    predictionAll = model.transform(AllRev)
+    predictionSentiment = groupedCat('prediction',predictionAll).withColumnRenamed('0.0','BadSentiment').withColumnRenamed('1.0','GoodSentiment')
+    predictionSentiment.show(10)
+    return predictionSentiment
+
 
 def GetReviewsData():
     config = getconfig()
@@ -55,9 +85,8 @@ def GetReviewsData():
     sqlC = SQLContext(sc)
     mongo_uri = f"{host}{db}.{rev}"
     rev = sqlC.read.format('com.mongodb.spark.sql.DefaultSource').option('uri',mongo_uri).load()
-    df = rev.createOrReplaceTempView("Rev")
     rev = rev.drop('CID').drop('_id').sort('ProdID')
-    ['ContentLocale', 'Date', 'IsFeatured', 'IsRecommended', 'IsSyndicated', 'ProdID', 'Rating', 'ReviewText', 'Title', 'age', 'eyeColor', 'hairColor', 'hairCondition', 'skinTone', 'skinType', 'url']
+    Sentiment = LogisticRegressions(rev)
     age = groupedCat('age',rev)
     ContentLocale = groupedCat("ContentLocale",rev)
     IsFeatured = groupedCat("IsFeatured",rev).withColumnRenamed('true','Featured').withColumnRenamed('false','NotFeatured')
@@ -77,10 +106,8 @@ def GetReviewsData():
                 .join(hairCondition.toDF(*[col + "_hair" for col in hairCondition.columns]).withColumnRenamed('ProdID_hair',"ProdID"),on='ProdID')\
                 .join(skinTone.toDF(*[col + "_skin" for col in skinTone.columns]).withColumnRenamed('ProdID_skin',"ProdID"),on='ProdID')\
                 .join(skinType.toDF(*[col + "_skin" for col in skinType.columns]).withColumnRenamed('ProdID_skin',"ProdID"),on='ProdID')\
-                
-    finalDt.toPandas().to_json('../../outputFiles/AggregateData.json')
-    sc.stop()
+                .join(Sentiment,on='ProdID')
     
+    finalDt.toPandas().to_json('../../outputFiles/dataset/AggregateData.json')
+    sc.stop()
     return None
-
-
